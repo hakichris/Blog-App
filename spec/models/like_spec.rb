5fd411f43255736971b1ac675c23c962565dccd6
Chris <hakizachrist1@gmail.com>
2023-05-12 17:43:21 +0200

    Add rspec files

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  subject { Like.new(author_id: 3, post_id: 3)}
+ 
+ 
+  context 'it should exist' do
+ it 'should exist as instance' do
+ expect(subject).to be_instance_of Like
+  end 
+end 
+end
