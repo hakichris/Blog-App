542767d83c17edbb53ff228fa16f66a83bf9bb03
Chris <hakizachrist1@gmail.com>
2023-05-18 09:24:00 +0200

    Update posts_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-end
+  end
 
   def show
     @user = User.find_by_id(params[:id])
-end
+    @post = Post.find_by_id(params[:id])
+  end
 end
