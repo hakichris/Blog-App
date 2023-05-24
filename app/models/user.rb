e2871fd5fe9abe4fcb7bfd6aaf421e78e6073db2
Chris <hakizachrist1@gmail.com>
2023-05-24 14:54:10 +0200

    Update devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
   validates :name, presence: true
   validates :postscounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
@@ -6,6 +9,10 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :posts, foreign_key: :author_id
 
+  before_validation :assignpostscounter
+  def assignpostscounter
+    self.postscounter = posts.count
+  end
   def recentthreeposts
     posts.order(created_at: :desc).limit(3)
   end
